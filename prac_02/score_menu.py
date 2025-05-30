08984cecd5f79146af5a62cdff9c11226df68891
34813 <junfei.chan@my.jcu.edu.au>
2025-05-30 16:33:08 +0800

    Add a function to get result from score

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,6 +1,3 @@
-from prac_02.password_stars import print_stars
-from prac_02.score import get_result
-
 MENU = """(G)et a valid score (must be 0-100 inclusive)
 (P)rint result (copy or import your function to determine the result from score.py)
 (S)how stars (this should print as many stars as the score)
@@ -8,7 +5,7 @@ MENU = """(G)et a valid score (must be 0-100 inclusive)
 
 def main():
     """main function"""
-    score = ""
+    score = get_score()
     print(MENU)
     choice = str(input(">>> ")).upper()
     while choice != "Q":
@@ -16,10 +13,7 @@ def main():
             score = get_score()
             print(f"Your score is {score}")
         elif choice == "P":
-            if score == "":
-                print("Please Enter your score first.")
-            else:
-                result = get_result(score)
+            result = get_result(score)
             print(result)
         elif choice == "S":
             print_stars(score)
@@ -31,9 +25,26 @@ def main():
 
 def get_score():
     """get and return a valid score"""
-    score = float(input("Enter score: "))
+    score = int(input("Enter score: "))
     while score <0 or score >100:
         print("Invalid score.")
-        score = float(input("Enter score: "))
+        score = int(input("Enter score: "))
     return score
 
+def get_result(score):
+    """get result from score"""
+    if score < 0:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score <= 90:
+        return "Passable"
+    elif score <= 100:
+        return "Excellent"
+    else:
+        return "Invalid score"
+
+def print_stars(score):
+    print("*" * score)
+
+main()
