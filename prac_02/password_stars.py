a12478b540b36ae2ed770dec507c32386ac8d532
34813 <junfei.chan@my.jcu.edu.au>
2025-05-30 15:46:44 +0800

    Move all the code inside a main

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,13 +2,9 @@ MIN_LENGTH = 6
 def main():
     """main function"""
     password = str(input("Enter password: "))
-    while is_valid_password(password) == False:
+    while len(password) < MIN_LENGTH:
         print("Invalid password.")
         password = str(input("Enter password: "))
     print("*" * len(password))
-
-def is_valid_password(password):
-    """check whether the password is valid"""
-    return len(password) >= MIN_LENGTH
 
 main()
