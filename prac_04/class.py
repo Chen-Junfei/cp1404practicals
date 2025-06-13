0fec9f9ed0b3312375a8e69c5ff35d3fee6b127c
34813 <junfei.chan@my.jcu.edu.au>
2025-06-13 15:24:29 +0800

    Copy the code from website for subject_data

--- prac_04/class.py
+++ prac_04/class.py
@@ -9,21 +9,42 @@
 #     print(",".join(names))
 #     name_to_remove = input("Who do you want to remove? ").strip()
 
-def main():
-    numbers = get_numbers()
-    square_numbers(numbers)
-    display_numbers(numbers)
 
-def get_numbers():
-    numbers = input("Enter numbers separated by commas: ").split(",")
-    return [float(number) for number in numbers]
+# def main():
+#     numbers = get_numbers()
+#     square_numbers(numbers)
+#     display_numbers(numbers)
+#
+# def get_numbers():
+#     numbers = input("Enter numbers separated by commas: ").split(",")
+#     return [float(number) for number in numbers]
+#
+# def square_numbers(numbers):
+#     for i in range(len(numbers)):
+#         numbers[i] = numbers[i] ** 2
+#
+# def display_numbers(numbers):
+#     sorted_numbers = sorted(numbers)
+#     print(sorted_numbers)
+#
+# main()
 
-def square_numbers(numbers):
-    for i in range(len(numbers)):
-        numbers[i] = numbers[i] ** 2
 
-def display_numbers(numbers):
-    sorted_numbers = sorted(numbers)
-    print(sorted_numbers)
+# data = [['Derek', 7], ['Xavier', 80], ['Bob', 612], ['Chantanelle', 9]]
+# max_len_of_name = max([len(pair[0]) for pair in data])
+# max_len_of_number = max([len(str(pair[1])) for pair in data])
+# for pair in data:
+#     name, score = pair
+#     print(f"{name:{max_len_of_name}}={score:{max_len_of_number}}")
 
-main()
+
+# print(isinstance(True,int))
+
+# values = [[3,4,5,6],[33,6,1,2]]
+# v = 3
+# print(values[0][0])
+# for row in range(len(values)):
+#     for column in range(len(values[row])):
+#         if v < values[row][column]:
+#             v = values[row][column]
+# print(v)
--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,12 +8,18 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
+    display_data(data)
+
+
+def display_data(data):
+    for row in range(len(data)):
+        print(f"{data[row][0]} is taught by {data[row][1]} and has {data[row][2]} students")
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
+    data = []
+    input_file = open(FILENAME, "r")
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +28,10 @@ def load_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        data.append(parts) # Add parts to data as element of list
         print("----------")
     input_file.close()
+    return data
 
 
 main()
