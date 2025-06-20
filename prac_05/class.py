87f6d1b426681740971893b78ce6b2bafcbcfd57
34813 <junfei.chan@my.jcu.edu.au>
2025-06-20 14:49:33 +0800

    Complete prac 5 email exercise and a class exercise in practical lecture

--- prac_05/class.py
+++ prac_05/class.py
@@ -1,5 +1,20 @@
-names = ["lukas", "sada", "adada"]
-names_to_length = {}
-for name in names:
-    names_to_length[name] = len(name)
-print(names_to_length)
+# def main():
+#     names = ["lukas", "sada", "adada"]
+#     names_to_length = convert_list_to_dictionary(names)
+#     print(names_to_length)
+#
+# def convert_list_to_dictionary(names):
+#     return {name: len(name) for name in names}
+#
+# main()
+
+
+# from operator import itemgetter
+#
+# names_to_scores = {'Derek': 7, 'Xavier': 80, 'Bob': 612, 'Chantanelle': 9}
+#
+# name_width = len(max(list(names_to_scores.keys()),key=len))
+# score_width = max([len(str(score)) for score in names_to_scores.values()])
+#
+# for name, score in sorted(names_to_scores.items(), key=itemgetter(1), reverse=True):
+#     print(f"{name:{name_width}} = {score:{score_width}}")
---
+++ prac_05/emails.py
@@ -0,0 +1,28 @@
+"""
+Word Occurrences
+Estimate: 20 minutes
+Actual:    minutes
+"""
+
+
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name(email)
+        choice = input(f"Is your name {name}? (Y/n) ")
+        if choice.upper() != "Y":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def get_name(email):
+    name = email.split("@")[0]
+    return ' '.join(name.split(".")).title()
+
+
+main()
+
