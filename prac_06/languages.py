e7bc6d28b170b97dc11c3ce07d55adad1d9991a5
34813 <junfei.chan@my.jcu.edu.au>
2025-07-06 10:12:26 +0800

    add comment for programming_language.py and complete guitar.py, guitar_test.py, and guitars.py

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,13 +1,14 @@
 """
 Word Occurrences
 Estimate: 5 minutes
-Actual:    minutes
+Actual:   13 minutes
 """
 
 from programming_language import ProgrammingLanguage
 
 
 def main():
+    """Create some ProgrammingLanguage objects and display their information."""
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,19 +1,25 @@
 """
 Word Occurrences
 Estimate: 10 minutes
-Actual:    minutes
+Actual:   3 minutes
 """
 
 class ProgrammingLanguage:
+    """Represent a ProgrammingLanguage object."""
+
+
     def __init__(self, name, typing, reflection, year):
+        """Construct a programming language with given name, typing, reflection and year."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
+        """Return string representation of a ProgrammingLanguage."""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
+        """Return True if the language is dynamically typed, False otherwise."""
         return self.typing == "Dynamic"
 
